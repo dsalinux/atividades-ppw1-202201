22c09ffd466a0bc4daf601dff8d5a116b2151304
Danilo Souza Almeida <dsalinux@gmail.com>
2022-06-15 21:58:12 -0300

    Finalização da calculadora.

--- Atividade22/js/script.js
+++ Atividade22/js/script.js
@@ -3,40 +3,73 @@ let numero2 = "";
 let operador = "";
 let resultado;
 
-document.querySelectorAll("a").forEach(link => {
+document.querySelectorAll(".calculadora .botoes a").forEach(link => {
     link.addEventListener("click", (event) => {
         event.preventDefault();
+        if(resultado) {
+            limpar();
+        }
         let valor = link.innerHTML;
-        console.log(link.innerHTML);
-        if(parseInt(valor) && operador === "") {
+        console.log(valor);
+        if (!isNaN(valor)  && operador === "") {
             numero1 = numero1 + link.innerHTML;
         }
-        if(parseInt(valor) && operador !== "") {
+        if (!isNaN(valor) && operador !== "") {
             numero2 = numero2 + link.innerHTML;
         }
-        if(valor === "+" || valor === "-" || valor === "*" || valor === "/") {
+        if (valor === "+" || valor === "-" || valor === "*" || valor === "÷") {
             console.log(valor);
             operador = valor;
+        }
+        if (valor === "=") {
+            switch (operador) {
+                case "+":
+                    resultado = parseInt(numero1) + parseInt(numero2);
+                    break;
+                case "-":
+                    resultado = parseInt(numero1) - parseInt(numero2);
+                    break;
+                case "*":
+                    resultado = parseInt(numero1) * parseInt(numero2);
+                    break;
+                case "÷":
+                    resultado = parseInt(numero1) / parseInt(numero2);
+                    break;
+            }
+        }
+        if (valor === "C") { 
+            limpar();
         }
         atulizarPainel();
     });
 });
 
+function limpar() {
+    numero1 = "";
+    numero2 = "";
+    operador = "";
+    resultado = "";
+}
 
-function atulizarPainel(){
+function atulizarPainel() {
     console.log(numero1);
     console.log(numero2);
     console.log(operador);
     let texto = "";
-    if(numero1) {
+    if (numero1) {
         texto += numero1;
     }
-    if(operador) {
+    if (operador) {
         texto += operador;
     }
-    if(numero2) {
+    if (numero2) {
         texto += numero2;
     }
-    document.querySelector(".painel").innerHTML = texto;
+    let result = "";
+    if(resultado){
+        result = resultado;
+    }
+    document.querySelector(".painel .resultado").innerHTML = result;
+    document.querySelector(".painel .operacao").innerHTML = texto;
     ;
 }
